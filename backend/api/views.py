fc7c614fe74e7f15ef900fabf422cd9207d10d4f
Av-sek <abhishekghorashainee07@gmail.com>
2022-06-06 20:28:47 +0545

    f

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,19 @@
-from email import header
+import imp
 from products.models import Product
-from django.http import JsonResponse,HttpResponse
 from django.forms.models import model_to_dict
-import json
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.serializers import ProductSerializer
+
 # Create your views here.
+@api_view(['GET','POST'])
 def api_home(request):
-    model_data = Product.objects.all().order_by('?').first()
+    """Drf Api View"""
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data,fields=['id','title','price'])
-    return HttpResponse(data,headers={'content-type':"application/json"})
+    if instance:
+        # data = model_to_dict(instance,fields=['id','title','price','sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data,headers={'content-type':"application/json"})
---
+++ backend/products/serializers.py
@@ -0,0 +1,17 @@
+from dataclasses import field
+from pyexpat import model
+from statistics import mode
+from rest_framework import serializers
+
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'content',
+            'price',
+            'sale_price',
+            'get_discount'
+        ]
